0e5739b77cc6d30c01af8a49090e8194dbb189a0
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-04-23 23:23:16 +0530

    Implemented UC1|UC2

---
+++ src/main/java/com/example/employee_payroll/User.java
@@ -0,0 +1,46 @@
+package com.example.employee_payroll;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @Id
+  private Long id ; 
+
+  @Column
+  private String firstname ; 
+
+  @Column
+  private String lastname ; 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+  
+}
